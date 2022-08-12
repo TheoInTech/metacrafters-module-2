f931a9d6be4986f6f8d85c2ae42d5b91125fb3b3
Theo Roque <theoroque95@gmail.com>
2022-08-12 15:58:43 +0800

    remove unnecessary files

--- packages/Challenge1-solana/__tests__/Challenge1-solana.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const challenge1Solana = require('..');
-
-describe('Challenge1-solana', () => {
-    it('needs tests');
-});
--- packages/Challenge1-solana/lib/Challenge1-solana.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = challenge1Solana;
-
-function challenge1Solana() {
-    // TODO
-}
